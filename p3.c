ab90b870f1115fb4caee6ac21981446c126b9ead
Sohaib Ahmad <145012326+sohaib4x@users.noreply.github.com>
2024-04-21 03:11:10 +0500

    Update p3.c

--- p3.c
+++ p3.c
@@ -1,1 +1,29 @@
+// pointers practice
 
+# include <stdio.h> 
+int main( ) 
+{ 
+ int i = 10, *ptr1 = &i;
+ int *ptr2 = ptr1;
+ *ptr2 = 30;
+ *ptr1 = 50;
+ i = 100;
+ 
+
+
+ printf("result = %d\n", i);
+  
+ printf("result = %d\n", *ptr1);
+  
+ printf("result = %d\n", *ptr2);
+
+ //printf("result = %d\n", *i); error
+  
+ printf("result = %x\n", ptr1);
+  
+ printf("result = %x\n", ptr2);
+
+ 
+  
+ //printf("result = %x\n", ptr);
+}
