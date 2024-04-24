4ece33e7ce183b181eaa9d2a5c43cfae240d57a0
Sohaib Ahmad <145012326+sohaib4x@users.noreply.github.com>
2024-04-24 18:44:29 +0500

    Create p10.c

---
+++ p10.c
@@ -0,0 +1,32 @@
+/*to print elements in reverse order*/
+#include <stdio.h>
+int main()
+{
+   int i, n, a[100];
+   printf("\n\nRead n number of values in an array and display it in reverse order:\n");
+   printf("------------------------------------------------------------------------\n");
+   // Prompt the user to input the number of elements to store in the array
+   printf("Input the number of elements to store in the array :");
+   scanf("%d", &n);
+   // Prompt the user to input n elements into the array
+   printf("Input %d number of elements in the array :\n", n);
+   for (i = 0; i < n; i++)
+   {
+      printf("element - %d : ", i);
+      scanf("%d  ", &a[i]);  // Read the input and store it in the array
+   }
+   // Display the values stored in the array
+   printf("\nThe values stored in the array are : \n");
+   for (i = 0; i < n; i++)
+   {
+       printf("%  d", a[i]);  // Print each element in the array
+   }
+   // Display the values stored in the array in reverse order
+   printf("\n\nThe values stored in the array in reverse are :\n");
+   for (i = n - 1; i >= 0; i--)
+   {
+       printf("%  d", a[i]);  // Print each element in reverse order
+   }
+   printf("\n\n");
+   return 0;
+}
